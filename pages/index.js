1dbf6180d4fba240d242ff77b779ed0fc179c4da
hyvip-ai <rm2932002@gmail.com>
2021-10-27 20:31:59 +0530

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 //ourdomain.com/
-import React,{useEffect,useState} from 'react'
-import MeetupList from '../components/MeetUps/MeetupList'
-import data from "../components/Dummy_Data/dummy"
-import Layout from '../components/layouts/Layout'
+import React, { useEffect, useState } from "react";
+import MeetupList from "../components/MeetUps/MeetupList";
+import data from "../components/Dummy_Data/dummy";
+import Layout from "../components/layouts/Layout";
 function HomePage() {
-    const [meetups, setmeetups] = useState([])
-    useEffect(() => {
-      setmeetups(data)
-    }, [])
-    return (
-        <>
-         
-        <Layout>
-           <MeetupList data={meetups}/>
-        </Layout></>
-    )
+  const [meetups, setmeetups] = useState([]);
+  useEffect(() => {
+    setmeetups(data);
+  }, []);
+  return (
+    <>
+      <Layout>
+        <MeetupList data={meetups} />
+      </Layout>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
