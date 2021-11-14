57b43a0753da24ba3e03c30f1631f07e6859a33f
hyvip-ai <rm2932002@gmail.com>
2021-11-14 12:26:30 +0530

    Initial Comiit

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,38 @@ import React, { useEffect, useState } from "react";
 import MeetupList from "../components/MeetUps/MeetupList";
 import data from "../components/Dummy_Data/dummy";
 import Layout from "../components/layouts/Layout";
-function HomePage() {
-  const [meetups, setmeetups] = useState([]);
-  useEffect(() => {
-    setmeetups(data);
-  }, []);
+function HomePage(props) {
+
   return (
     <>
       <Layout>
-        <MeetupList data={meetups} />
+        <MeetupList data={props.meetups} />
       </Layout>
     </>
   );
 }
+
+export async function getStaticProps(){
+  //fetch data from api
+  //the  return an object specificly object
+  return {
+    props:{
+      meetups:data
+    },
+    revalidate:10
+  }
+  //home page recevied the object which is the value of the key props
+};
+
+// export function getServerSideProps(context){
+//   const req = context.req;
+//   const res = context.res;
+
+//   return { 
+//     props:{
+//       meetups:data
+//     }
+//   }
+// }
 
 export default HomePage;
