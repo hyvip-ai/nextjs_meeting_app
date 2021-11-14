b382c6f5553a4870bed4c7d480aafec546c73e4e
hyvip-ai <rm2932002@gmail.com>
2021-11-14 14:37:13 +0530

    Added static path fro ssg

--- pages/[meetUpId].js
+++ pages/[meetUpId].js
@@ -3,15 +3,45 @@ import data from "../components/Dummy_Data/dummy"
 import {useRouter} from "next/router"
 import MeetUpDetailsById from '../components/MeetUps/MeetUpDetails';
 import Layout from '../components/layouts/Layout';
-function MeetUpDetails() {
-    const {image,title,description,address} = data[0];
-    const {meetUpId} = useRouter().query;
-    console.log(meetUpId)
+function MeetUpDetails(props) {
+    // const {meetUpId} = useRouter().query;
+    // console.log(meetUpId)
     return (
         <Layout>
-      <MeetUpDetailsById image={image} title={title} description = {description} address={address}/>
+      <MeetUpDetailsById image={data[0].image} title={data[0].title} description = {data[0].description} address={data[0].address}/>
       </Layout>
     )
+}
+export function getStaticPaths(){
+  return {
+    paths:[
+      {
+        params:{
+          meetUpId:'m1'
+        }
+      },
+      {
+        params:{
+          meetUpId:'m2'
+        }
+      }
+    ],
+    fallback:false
+  }
+}
+
+export function getStaticProps(context){
+ const meetupId =  context.params.meetUpId
+ console.log(meetupId)
+  return {
+    props:{
+      id:'m1',
+      image:data[0].image,
+      title:data[0].title,
+      description:data[0].description,
+      address:data[0].description
+    }
+  }
 }
 
 export default MeetUpDetails
